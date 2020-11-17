824426634f1fcd77046eb65ab7ee4495de57b27f
AvalanZ <frogcrose@gmail.com>
2020-11-17 13:44:33 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ckqpjv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ckqpjv)
---
+++ src/app/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyAYHg3jmkh3usybPeu7nd8ObvyPGLuLdyo",
+    authDomain: "tweety-cab34.firebaseapp.com",
+    databaseURL: "https://tweety-cab34.firebaseio.com",
+    projectId: "tweety-cab34",
+    storageBucket: "tweety-cab34.appspot.com",
+    messagingSenderId: "32327038814",
+    appId: "1:32327038814:web:a0963c470b37077a8341cf",
+    measurementId: "G-9TJC485HX1"
+  }
+}
